bd9b2763fd74df67f23b54acd8427df20072a739
Juliano Almada <almadaj@edu.unifor.br>
2023-11-20 13:19:10 -0300

    validando métodos com postman

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Minha lista de tarefas</h1>");
+});
+
+app.get("/json", (req, res) => {
+  res.json({ title: "Tarefa X", done: true });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado");
+});
