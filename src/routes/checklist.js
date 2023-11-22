d6d015bff592f9ef39e06a498f313932fe751e7b
Juliano Almada <almadaj@edu.unifor.br>
2023-11-22 16:20:11 -0300

    rotas atualizadas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,34 +2,54 @@ const express = require("express");
 const router = express.Router();
 const Checklist = require("../models/checklist");
 
-router.get("/", (req, res) => {
-  console.log("Olá");
-  res.send();
+router.get("/", async (req, res) => {
+  try {
+    let checklists = await Checklist.find({});
+    res.status(200).json(checklists);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post("/", async (req, res) => {
   let { name } = req.body;
   try {
-    let checklist = await Checklist.create({ name });
+    let checklists = await Checklist.create({ name });
+    res.status(200).json(checklists);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
     res.status(200).json(checklist);
   } catch (error) {
     res.status(422).json(error);
   }
 });
 
-router.get("/:id", (req, res) => {
-  console.log(req.body["name"]);
-  res.send(`ID: ${req.params.id}`);
+router.put("/:id", async (req, res) => {
+  let { name } = req.body;
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(
+      req.params.id,
+      { name },
+      { new: true }
+    );
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  console.log(req.params.id);
-  res.send(`PUT: ${req.params.id}`);
+router.delete("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndRemove(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
-
-router.delete("/:id", (req, res) => {
-  console.log(req.params.id);
-  res.send(`DELETE: ${req.params.id}`);
-});
-
 module.exports = router;
