4e0dbb668f847854b0905376287f31cf2da44f00
Juliano Almada <almadaj@edu.unifor.br>
2023-11-23 14:59:01 -0300

    estabelecendo dados através das rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,21 +1,26 @@
 const express = require("express");
+
 const router = express.Router();
+
 const Checklist = require("../models/checklist");
 
 router.get("/", async (req, res) => {
   try {
     let checklists = await Checklist.find({});
-    res.status(200).json(checklists);
+    res.status(200).render("checklists/index", { checklists: checklists });
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(422)
+      .render("pages/error", { error: "Erro ao exibir as Listas" });
   }
 });
 
 router.post("/", async (req, res) => {
   let { name } = req.body;
+
   try {
-    let checklists = await Checklist.create({ name });
-    res.status(200).json(checklists);
+    let checklist = await Checklist.create({ name });
+    res.status(200).json(checklist);
   } catch (error) {
     res.status(422).json(error);
   }
@@ -24,14 +29,17 @@ router.post("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id);
-    res.status(200).json(checklist);
+    res.status(422).render("checklists/show", { checklist: checklist });
   } catch (error) {
-    res.status(422).json(error);
+    res
+      .status(200)
+      .render("pages/error", { error: "Erro ao exibir as Listas de tarefas" });
   }
 });
 
 router.put("/:id", async (req, res) => {
   let { name } = req.body;
+
   try {
     let checklist = await Checklist.findByIdAndUpdate(
       req.params.id,
@@ -52,4 +60,5 @@ router.delete("/:id", async (req, res) => {
     res.status(422).json(error);
   }
 });
+
 module.exports = router;
