6aeea6f4c601c5ab21f3c17ccf9ed9faa0d685e6
Juliano Almada <almadaj@edu.unifor.br>
2023-11-23 00:49:25 -0300

    views criadas

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const path = require("path");
 const checkListRouter = require("./src/routes/checklist");
+const rootRouter = require("./src/routes/index");
 require("./config/database");
 
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
 app.use("/checklists", checkListRouter);
+app.use("/", rootRouter);
 
 app.listen(3000, () => {
   console.log("Servidor foi iniciado");
