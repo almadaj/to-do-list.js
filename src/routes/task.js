e01013879bfd7bbc1982d72397795f94a841c4bc
Juliano Almada <almadaj@edu.unifor.br>
2023-11-24 23:11:09 -0300

    to-do list finalizado

--- src/routes/task.js
+++ src/routes/task.js
@@ -15,7 +15,7 @@ checklistDepedentRoute.get("/:id/tasks/new", async (req, res) => {
   } catch (error) {
     res
       .status(422)
-      .render("pages/error", { errors: "Erro ao carregar o formulário" });
+      .render("/pages/error", { erros: "Erro ao carregar o formulário" });
   }
 });
 
@@ -23,36 +23,48 @@ simpleRouter.delete("/:id", async (req, res) => {
   try {
     let task = await Task.findByIdAndDelete(req.params.id);
     let checklist = await Checklist.findById(task.checklist);
-    let taskToRemove = checklist.tasks.indexOf(task._id);
-    checklist.tasks.slice(taskToRemove, 1);
+    let taskToRemove = checklist.task.indexOf(task._id);
+    checklist.task.slice(taskToRemove, 1);
     checklist.save();
-    res.redirect(`/checklists/${checklist._id}`);
+    res.redirect(`/checklist/${checklist._id}`);
   } catch (error) {
     res
       .status(422)
-      .render("pages/error", { errors: "Erro ao remover uma tarefa" });
+      .render("/pages/error", { erros: "Erro ao remover uma tarefa" });
   }
 });
 
 checklistDepedentRoute.post("/:id/tasks", async (req, res) => {
   let { name } = req.body.task;
   let task = new Task({ name, checklist: req.params.id });
-
   try {
     await task.save();
     let checklist = await Checklist.findById(req.params.id);
-    checklist.tasks.push(task);
+    checklist.task.push(task);
     await checklist.save();
-    res.redirect(`/checklists/${req.params.id}`);
+    res.redirect(`/checklist/${req.params.id}`);
   } catch (error) {
     let errors = error.errors;
-    res.status(422).render("tasks/new", {
-      task: { ...task, errors },
-      checklistId: req.params.id,
-    });
+    res
+      .status(422)
+      .render("tasks/new", {
+        task: { ...task, errors },
+        checklistId: req.params.id,
+      });
   }
 });
 
+simpleRouter.put("/:id", async (req, res) => {
+  let task = await Task.findById(req.params.id);
+  try {
+    task.set(req.body.task);
+    await task.save();
+    res.status(200).json({ task });
+  } catch (error) {
+    let errors = error.erros;
+    res.status(422).json({ task: { ...errors } });
+  }
+});
 module.exports = {
   checklistDepedent: checklistDepedentRoute,
   simple: simpleRouter,
